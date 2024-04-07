d8b2fd65eddeeec167c42eaa863632b6c733d548
hannah mathew <hannah.mathew@stonybrook.edu>
2024-04-07 19:30:00 -0400

    My second submission

--- homework_question1.c
+++ homework_question1.c
@@ -1,1 +1,54 @@
+/*hannah mathew 115441475*/
 
+#include <stdio.h>
+#include <string.h>
+
+int isVowel(char c) {
+    c = tolower(c); 
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
+
+char* ToPigLatin(char* word) {
+    static char pigLatin[40]; 
+    int len = strlen(word);
+    int i;
+
+    if (isVowel(word[0])) {
+        strcpy(pigLatin, word);
+        strcat(pigLatin, "way");
+    } else { 
+        for (i = 0; i < len; i++) {
+            if (isVowel(word[i]))
+                break;
+        }
+        strcpy(pigLatin, word + i);
+        strncat(pigLatin, word, i);
+        strcat(pigLatin, "ay");
+    }
+
+    if (word[0] >= 'A' && word[0] <= 'Z') {
+        pigLatin[0] = pigLatin[0] - 'a' + 'A'; 
+    }
+
+    return pigLatin;
+}
+
+int main() {
+    char words[5][40];
+    char pigLatin[5][40];
+    int i;
+
+    printf("Input 5 words: ");
+    for (i = 0; i < 5; i++) {
+        scanf("%s", words[i]);
+    }
+
+    printf("Pig Latin version of the 5 words:\n");
+    for (i = 0; i < 5; i++) {
+        strcpy(pigLatin[i], ToPigLatin(words[i]));
+        printf("%s ", pigLatin[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
--- part3_question2.c
+++ part3_question2.c
@@ -1,1 +1,41 @@
+/*hannah mathew 115441475*/
+#include <stdio.h>
+#include <string.h>
 
+int removeChars(char s1[], char s2[]) {
+    int i, j;
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+
+    for (i = 0; i < len2; i++) {
+        for (j = 0; j < len1; j++) {
+            if (s1[j] == s2[i]) {
+                for (int k = j; k < len1 - 1; k++) {
+                    s1[k] = s1[k + 1];
+                }
+                len1--;
+                j--;
+            }
+        }
+    }
+
+    return len1;
+}
+
+int main() {
+    char s1[100], s2[100];
+
+    printf("Enter string s1: ");
+    fgets(s1, sizeof(s1), stdin);
+    printf("Enter string s2: ");
+    fgets(s2, sizeof(s2), stdin);
+
+    s1[strcspn(s1, "\n")] = '\0';
+    s2[strcspn(s2, "\n")] = '\0';
+
+    int newLength = removeChars(s1, s2);
+
+    printf("Output: %s\n", s1);
+
+    return 0;
+}
